93a8be7c4ad04d9b3f55f53e2d6f148f71856239
Kevin Mueller <kevin.mueller@tuta.com>
2025-02-17 06:23:20 +0100

    changed model

--- orders_app/models.py
+++ orders_app/models.py
@@ -30,14 +30,14 @@ class Orders(models.Model):
         'completed': 'completed',
         'cancelled': 'cancelled'
     }
-    customer_user = models.ForeignKey(User, on_delete=models.CASCADE)
-    business_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer_user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='customer_user_order')
+    business_user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='business_user_order')
     title = models.CharField(max_length=100, blank=False)
     revisions = models.IntegerField(null=False)
     delivery_time_in_days = models.IntegerField(null=False)
     price = models.FloatField(null=False)
     features = models.JSONField(default=dict, null=False, blank=False)
-    offer_type = models.CharField(choices=TYPES_CHOICE)
-    status = models.CharField(choices=STATUS_CHOICE)
+    offer_type = models.CharField(choices=TYPES_CHOICE, max_length=20)
+    status = models.CharField(choices=STATUS_CHOICE, max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
