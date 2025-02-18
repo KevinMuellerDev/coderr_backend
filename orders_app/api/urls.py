a47c0cb6c92a71cac5aa69c5296de80bcf3cf7d7
Kevin Mueller <kevin.mueller@tuta.com>
2025-02-18 09:54:06 +0100

    added completed-order-count url

--- orders_app/api/urls.py
+++ orders_app/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
-from orders_app.api.views import OrdersViewset,OrderCount
+from orders_app.api.views import OrdersViewset,OrderCount,OrderCompletedCount
 
 router = DefaultRouter()
 router.register(r'orders',OrdersViewset)
 
 urlpatterns = [
     path('',include(router.urls)),
-    path('order-count/<int:business_user_id>/',OrderCount.as_view(),name='order_count')
+    path('order-count/<int:business_user_id>/',OrderCount.as_view(),name='order_count'),
+    path('completed-order-count/<int:business_user_id>/',OrderCompletedCount.as_view(),name='completed_order_count')
 ]
